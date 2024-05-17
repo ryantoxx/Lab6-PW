212359e9682db2225696181957c03d6f60348fab
ryantoxx <nikitachihai2@gmail.com>
2024-05-17 09:05:57 +0300

    welcome page

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,14 @@
+// WelcomePage.js
+import React from 'react';
+
+const WelcomePage = () => {
+  return (
+    <div className="welcome-page">
+      <h1>Welcome to Recipe Web App</h1>
+      <p>This is a place where you can find delicious recipes for your favorite dishes.</p>
+      <p>Start exploring now by searching for different recipes!</p>
+    </div>
+  );
+};
+
+export default WelcomePage;
