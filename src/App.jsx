b3564bbc04782595919269b3726edf055f413b82
ryantoxx <nikitachihai2@gmail.com>
2024-05-17 09:19:43 +0300

    add api to the web page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import WelcomePage from './components/WelcomePage';
+import RecipeSearch from './components/RecipeSearch';
 import './App.css';
 
 function App() {
   const [showWelcomePage, setShowWelcomePage] = useState(true);
+  const [addedRecipes, setAddedRecipes] = useState([]);
 
   const handleDismissWelcome = () => {
     setShowWelcomePage(false);
   };
 
+  const updateAddedRecipes = (recipeToAdd) => {
+    setAddedRecipes(prevAddedRecipes => [...prevAddedRecipes, { title: recipeToAdd }]);
+  };
+
+  const updateAddedRecipesToRemove = (recipeToRemove) => {
+    setAddedRecipes(prevAddedRecipes => prevAddedRecipes.filter(recipe => recipe.title !== recipeToRemove));
+  };
+
 
   return (
+    <div className={`App`}>
+      {showWelcomePage ? (
         <WelcomePage />
-  )
+      ) : (
+        <div>
+          <h1>Recipe Search</h1>
+          <RecipeSearch updateAddedRecipes={updateAddedRecipes} updateAddedRecipesToRemove={updateAddedRecipesToRemove} />
+        </div>
+      )}
+      {showWelcomePage && (
+        <button className="start-exploring-button" onClick={handleDismissWelcome}>Start Exploring</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/RecipeActions.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const RecipeActions = ({ recipe, updateAddedRecipes }) => {
+  const [added, setAdded] = useState(localStorage.getItem(`added_${recipe.title}`) === 'true');
+
+  const toggleLike = () => {
+    const newAdded = !added;
+    setAdded(newAdded);
+    localStorage.setItem(`added_${recipe.title}`, newAdded);
+    if (newAdded) {
+      updateAddedRecipes(recipe.title); 
+    } else {
+      updateAddedRecipesToRemove(recipe.title); 
+    }
+  };
+
+  return (
+    <div>
+      <button className="add-button" onClick={toggleLike} style={{ color: added ? 'white' : 'white' }}>
+        {added ? 'Delete' : 'Add'}
+      </button>
+    </div>
+  );
+};
+
+export default RecipeActions;
+
+
+
