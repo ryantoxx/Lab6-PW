1481adceda3f16a2f4713bfcbbe2d767ba5cb182
ryantoxx <nikitachihai2@gmail.com>
2024-05-17 09:42:29 +0300

    add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import React, { useState, useEffect } from 'react';
 import WelcomePage from './components/WelcomePage';
 import RecipeSearch from './components/RecipeSearch';
 import PersonalCabinet from './components/PersonalCabinet';
+import ThemeToggle from './components/ThemeToggle';
 import './App.css';
 
 function App() {
   const [showWelcomePage, setShowWelcomePage] = useState(true);
   const [addedRecipes, setAddedRecipes] = useState([]);
   const [showPersonalCabinet, setShowPersonalCabinet] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    const isDark = storedTheme === 'dark';
+    setIsDarkMode(isDark);
+    if (isDark) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, []);
 
   const handleDismissWelcome = () => {
     setShowWelcomePage(false);
@@ -25,9 +38,23 @@ function App() {
     setShowPersonalCabinet(prevState => !prevState);
   };
 
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode ? 'dark' : 'light';
+    setIsDarkMode(!isDarkMode);
+    localStorage.setItem('theme', newTheme);
+
+    if (newTheme === 'dark') {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+  };
 
   return (
-    <div className={`App`}>
+    <div className={`App ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      <ThemeToggle toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
       {showWelcomePage ? (
         <WelcomePage />
       ) : (
