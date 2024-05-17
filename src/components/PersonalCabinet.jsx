3f4de842af6c43971dbcaf09ebc3dcae6c39b936
ryantoxx <nikitachihai2@gmail.com>
2024-05-17 10:13:05 +0300

    remove redundant code

--- src/components/PersonalCabinet.jsx
+++ src/components/PersonalCabinet.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 
 const PersonalCabinet = () => {
-  // const [addedRecipes, setAddedRecipes] = React.useState(getAddedRecipes());
-
-  // function getAddedRecipes() {
-  //   return Object.keys(localStorage)
-  //     .filter(key => key.startsWith('added_'))
-  //     .map(key => ({
-  //       title: key.replace('added_', ''),
-  //       liked: localStorage.getItem(key) === 'true'
-  //     }));
-  // }
   const getAddedRecipes = Object.keys(localStorage)
     .filter(key => key.startsWith('added_') && localStorage.getItem(key) === "true")
     .map(key => key.replace('added_', ''));
